e46b94fe1c436073d5b73cfd8387135359b06ef1
ks-155 <158799414+ks-155@users.noreply.github.com>
2025-08-13 17:58:31 +0530

    Add files via upload

---
+++ ALLMATHOP.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("Enter Two Numbers:");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("%d+%d=%d\n",a,b,c);
+c=a-b;
+printf("%d-%d=%d\n",a,b,c);
+c=a/b;
+printf("%d/%d=%d\n",a,b,c);
+c=a*b;
+printf("%d*%d=%d\n",a,b,c);
+getch();
+}
