9787522d84f1c5cc2198c55e8ff35403c10a57b3
ks-155 <158799414+ks-155@users.noreply.github.com>
2025-08-20 17:29:25 +0530

    Add files via upload

---
+++ using for loop printing first 10 natural numbers.c
@@ -0,0 +1,8 @@
+/*printing 1 to 10 numbers using a for loop*/
+#include<stdio.h>
+int main()
+{printf("printing 1 to 10 numbers using a for loop:\n");
+for(int i=1;i<=10;i++)
+{printf("%d\n",i);}
+getch();
+}
