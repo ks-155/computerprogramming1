9e70dc35b2fcf9e9f178e7dc62612a4eaaf174d4
ks-155 <158799414+ks-155@users.noreply.github.com>
2025-08-20 17:05:12 +0530

    Add files via upload

---
+++ greatest and lowest of two values.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int num1,num2;
+printf("Enter the first value:");
+scanf("%d",&num1);
+printf("Enter the second value:");
+scanf("%d",&num2);
+if(num1>num2)
+{printf("the largest number is:%d\n",num1);}
+else {printf("the largest number is:%d\n",num2);}
+getch();
+}
+
