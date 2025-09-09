f00e9f670018309a09cd69d72e61eeb5d6c5daf3
ks-155 <158799414+ks-155@users.noreply.github.com>
2025-09-09 22:40:28 +0530

    Create q25_Check whether a given number is a palindrome or not.c

---
+++ q25_Check whether a given number is a palindrome or not.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num, original_num, reversed_num = 0, digit;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    original_num = num; // Store the original number
+
+    // Reverse the number
+    while (num != 0) {
+        digit = num % 10;
+        reversed_num = reversed_num * 10 + digit;
+        num = num / 10;
+    }
+
+    // Check if the original number and reversed number are the same
+    if (original_num == reversed_num) {
+        printf("%d is a palindrome.\n", original_num);
+    } else {
+        printf("%d is not a palindrome.\n", original_num);
+    }
+
+    return 0;
+}
