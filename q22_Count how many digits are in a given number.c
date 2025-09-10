a56a66c0a739e4b23aa3c832ccb950cc678d34bc
ks-155 <158799414+ks-155@users.noreply.github.com>
2025-09-10 17:25:52 +0530

    Update q22_Count how many digits are in a given number.c

--- q22_Count how many digits are in a given number.c
+++ q22_Count how many digits are in a given number.c
@@ -7,15 +7,15 @@ int main() {
     printf("Enter an integer: ");
     scanf("%d", &num);
 
-    // Special case for 0, which has one digit
+ 
     if (num == 0) {
         count = 1;
     } else {
-        // Loop until the number becomes 0
+    
         while (num != 0) {
-            // Remove the last digit
+           
             num = num / 10;
-            // Increment the digit counter
+           
             count++;
         }
     }
