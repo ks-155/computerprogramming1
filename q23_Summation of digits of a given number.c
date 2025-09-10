ac755511a3ff11e4cda14d81c81ad7a7c97a85b2
ks-155 <158799414+ks-155@users.noreply.github.com>
2025-09-10 17:30:47 +0530

    Update q23_Summation of digits of a given number.c

--- q23_Summation of digits of a given number.c
+++ q23_Summation of digits of a given number.c
@@ -6,13 +6,13 @@ int main() {
     printf("Enter an integer: ");
     scanf("%d", &num);
 
-    // Loop until the number becomes 0
+   
     while (num != 0) {
-        // Get the last digit
+        
         digit = num % 10;
-        // Add the digit to sum
+       
         sum = sum + digit;
-        // Remove the last digit
+        
         num = num / 10;
     }
 
