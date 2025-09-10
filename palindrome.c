ca73f8253ae008f97f8e5bc1d97578f89d42e494
ks-155 <158799414+ks-155@users.noreply.github.com>
2025-09-10 17:17:08 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+    int num, originalNum, reversedNum = 0, digit;
+
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+
+    originalNum = num;
+
+
+    printf("Separated digits: ");
+
+
+    while (num == 0) {
+
+        digit = num % 10;
+        printf("%d ", digit);
+       reversedNum = reversedNum * 10 + digit;
+
+       num = num / 10;
+    }
+
+    printf("\n");
+
+    if (originalNum == reversedNum) {
+        printf("The number %d is a palindrome.\n", originalNum);
+    } else {
+        printf("The number %d is NOT a palindrome.\n", originalNum);
+    }
+
+    return 0;
+}
